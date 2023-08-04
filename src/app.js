2b66174be1ef158f419dac61398029b5e54c9ffd
Anand Rajaram <anandrajaram21@gmail.com>
2023-08-04 20:55:17 +0530

    feat: add a working app home page

--- src/app.js
+++ src/app.js
@@ -1,17 +1,26 @@
 const { App } = require('@slack/bolt');
 const dotenv = require('dotenv');
+const home = require('./user-interface/home');
+
+// console.log(home());
 
 dotenv.config();
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
-  appToken: process.env.SLACK_APP_TOKEN
+  signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-app.message('hello', async ({ message, say }) => {
-  await say(`Hey there <@${message.user}>!`);
+app.event('app_home_opened', async ({ event, client }) => {
+  await client.views.publish({
+    user_id: event.user,
+    view: home()
+  });
+});
+
+app.action('button_click', async ({ body, ack, say }) => {
+  await ack();
+  await say(`<@${body.user.id}> clicked the button`);
 });
 
 (async () => {
